0c77cd3d2edec8a95aa925154074f7a3412b821b
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2023-11-07 15:44:36 +0530

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const background = document.getElementsByClassName("list-group-item");
-background[2].style.backgroundColor = "green";
-for(var i =0;i<background.length;i++){
-    background[i].style.fontWeight = 'bold';
-}
+// const background = document.getElementsByClassName("list-group-item");
+// background[2].style.backgroundColor = "green";
+// for(var i =0;i<background.length;i++){
+//     background[i].style.fontWeight = 'bold';
+// }
+const item = document.getElementsByTagName('li');
+for(var i =0;i<item.length;i++){
+      item[i].style.backgroundColor = '#f4f4f4'
+    }
