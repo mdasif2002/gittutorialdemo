7fb77786a673b0bfd89fe4f096b6661c4ecb348f
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2023-11-07 16:05:08 +0530

    Third commit

--- app.js
+++ app.js
@@ -3,7 +3,23 @@
 // for(var i =0;i<background.length;i++){
 //     background[i].style.fontWeight = 'bold';
 // }
-const item = document.getElementsByTagName('li');
-for(var i =0;i<item.length;i++){
-      item[i].style.backgroundColor = '#f4f4f4'
+// const item = document.getElementsByTagName('li');
+// for(var i =0;i<item.length;i++){
+//       item[i].style.backgroundColor = '#f4f4f4'
+//     }
+
+//QuerySelector
+const que = document.querySelector('.list-group-item:nth-child(2)');
+que.style.backgroundColor='green'
+
+const third = document.querySelector('.list-group-item:nth-child(3)');
+third.style.display = 'none'
+
+//QuerySelectorAll
+const lists = document.querySelectorAll('li');
+lists[1].style.color='green'
+for(var i=0;i<lists.length;i++){
+    if(lists[i]%2!=0){
+        lists[i].style.backgroundColor='green'
     }
+}
