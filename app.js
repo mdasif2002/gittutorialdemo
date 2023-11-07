9d51339228fec5eba9fd78b808c54971b36b9da3
iqubal0612 <116081491+iqubal0612@users.noreply.github.com>
2023-11-07 15:01:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const background = document.getElementsByClassName("list-group-item");
+background[2].style.backgroundColor = "green";
+for(var i =0;i<background.length;i++){
+    background[i].style.fontWeight = 'bold';
+}
